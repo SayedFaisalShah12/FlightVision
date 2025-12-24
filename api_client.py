cd624847cee1e1e62ab63589b6bd36592720794f
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-12-24 22:54:41 +0500

    update

--- api_client.py
+++ api_client.py
@@ -1,6 +1,16 @@
 import requests
 from config import API_KEY
 
+BASE_URL = "https://api.aviationstack.com/v1/flights"
+
+def get_live_flights(limit=5):
+    params = {
+        "access_key": API_KEY,
+        "limit": limit
+    }
+    response = requests.get(BASE_URL, params=params)
+    return response.json()["data"]
+
 def get_airport_coordinates(iata_code):
     url = "https://api.aviationstack.com/v1/airports"
     params = {
@@ -15,4 +25,3 @@ def get_airport_coordinates(iata_code):
         return float(data[0]["latitude"]), float(data[0]["longitude"])
 
     return None, None
-
